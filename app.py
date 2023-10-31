81769ee4b4b488923557cc5e85f6a9f658c24983
Mnhz <140340472+Ethxmnhz@users.noreply.github.com>
2023-10-31 11:11:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, jsonify
+import openai
+
+app = Flask(__name__)
+openai.api_key = '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'
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    user_input = request.args.get('msg')
+
+    # Use OpenAI API for chatbot response
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=f"You: {user_input}\nBot:",
+        max_tokens=150 
+    )
+
+    bot_message = response['choices'][0]['text'].strip()
+
+    return bot_message
+
+if __name__ == "__main__":
+    app.run(debug=True)
